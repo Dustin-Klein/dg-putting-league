7955302d0979f30fb7e523fd672b26fe73c694d1
Dustin-Klein <dbklein4@gmail.com>
2026-01-13 19:17:03 -0600

    add league loading page

---
+++ app/league/[leagueId]/loading.tsx
@@ -0,0 +1,57 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
+export default function Loading() {
+  return (
+    <div className="container mx-auto p-4">
+      <div className="mb-6">
+        {/* Back link skeleton */}
+        <Skeleton className="h-5 w-32 mb-4" />
+
+        {/* League name and city */}
+        <div className="mb-6">
+          <Skeleton className="h-8 w-48 mb-2" />
+          <Skeleton className="h-5 w-32" />
+        </div>
+
+        {/* Events grid */}
+        <div className="space-y-4">
+          <div className="flex justify-end">
+            <Skeleton className="h-10 w-32" />
+          </div>
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {[1, 2, 3].map((i) => (
+              <div key={i} className="border rounded-lg p-4">
+                <div className="flex justify-between items-start">
+                  <div className="space-y-2">
+                    <Skeleton className="h-6 w-32" />
+                    <Skeleton className="h-4 w-24" />
+                  </div>
+                  <Skeleton className="h-5 w-16 rounded-full" />
+                </div>
+
+                <div className="mt-4 grid grid-cols-2 gap-4">
+                  <div className="space-y-1">
+                    <Skeleton className="h-4 w-12" />
+                    <Skeleton className="h-5 w-8" />
+                  </div>
+                  <div className="space-y-1">
+                    <Skeleton className="h-4 w-16" />
+                    <Skeleton className="h-5 w-10" />
+                  </div>
+                  <div className="col-span-2 space-y-1">
+                    <Skeleton className="h-4 w-20" />
+                    <Skeleton className="h-5 w-24" />
+                  </div>
+                </div>
+
+                <div className="mt-4">
+                  <Skeleton className="h-9 w-24" />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
